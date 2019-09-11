a50b4d7e15f1f1821ebcb3501e52f4a0a8618c83
PopAndrei <popandrew90@gmail.com>
2019-09-11 20:51:22 +0300

    First Commitment

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        //Types of data
+        String firstname = "Andrei";
+        String lastname = "Pop";
+        byte age = 29;
+        float hight = 1.91f;
+
+        System.out.println(firstname, lastname,\new);
+
+
+
+    }
+}

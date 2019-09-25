b7d9eec00cd1b3f778e42f9fcfff2585c4b16af6
PopAndrei <popandrew90@gmail.com>
2019-09-25 11:23:18 +0300

    Metode

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -3,19 +3,19 @@ package lab2;
 public class Main {
     public static void main(String[] args) {
 
-      int x = 24;
+      float x = 24.5f;
       int y = 4;
-      int sum = sum(x,y);
+      float sum = sum(x,y);
         System.out.println("The result of the sum is: " + sum);
 
-      int subtraction = subtraction(x,y);
+ /*     int subtraction = subtraction(x,y);
         System.out.println("The result of the subtraction is: " + subtraction);
 
       int multiply = multiply(x,y);
         System.out.println("The result of the multiplication is: " + multiply);
 
       int divide = divide(x,y);
-        System.out.println("The result of the division is: " + divide);
+        System.out.println("The result of the division is: " + divide);*/
 
 
     }

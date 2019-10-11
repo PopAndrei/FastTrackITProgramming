2234f35cae09bdeb4525c7d8bfa6db96b21c165a
PopAndrei <popandrew90@gmail.com>
2019-10-11 17:19:37 +0300

    Metode

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -3,10 +3,25 @@ package lab2;
 public class Main {
     public static void main(String[] args) {
 
+        salut();
+
+
       float x = 24.5f;
       int y = 4;
       float sum = sum(x,y);
-        System.out.println("The result of the sum is: " + sum);
+        System.out.println("2. The result of the sum is: " + sum);
+
+        x = 10;
+        y = 3;
+        System.out.println("3. " + divide(x,y));
+
+        System.out.println("4. " + "\n" + "\t" + "a." + punctulA(5,8,6) + "\n" + "\t" + "b." + punctulB(55,9) +
+                "\n" + "\t" + "c." +punctulC(20,3,5,8) +
+                "\n" + "\t" + "d." + punctulD(5,15,3,2,8));
+
+
+
+
 
  /*     int subtraction = subtraction(x,y);
         System.out.println("The result of the subtraction is: " + subtraction);
@@ -20,6 +35,12 @@ public class Main {
 
     }
 
+    public static void salut() {
+        System.out.println("Hello" + "\n" + "Andrei");
+    }
+
+
+
     public static int sum (int first, int second) {
         int rez = first + second;
         return rez;
@@ -27,10 +48,13 @@ public class Main {
     public static float sum (float first, float second){
         return first + second;
     }
+
     public static int subtraction (int first, int second) {
         int rez = first - second;
         return rez;
     }
+    public static float subtraction (float first, float second){ return first - second; }
+
     public static int multiply (int first, int second) {
         int rez = first * second;
         return rez;
@@ -39,4 +63,11 @@ public class Main {
         int rez = first / second;
         return rez;
     }
+    public static float divide (float first, float second) { return first/second; }
+
+    public static int punctulA (int first, int second, int third) { return -first + second * third;    }
+
+    public static int punctulB (int first, int second) { return (first +second) % second;}
+    public static float punctulC (float first, float second, float third, float fourth) { return first + (-second * third) / fourth;}
+    public static int punctulD (int first, int second, int third, int fourth, int fifth) {return first + second / third * fourth - fifth % third;}
 }

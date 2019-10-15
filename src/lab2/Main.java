eee2e0c2988d3d820efaa797aa2bf4caf523a990
PopAndrei <popandrew90@gmail.com>
2019-10-15 16:41:53 +0300

    Metode

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -3,28 +3,33 @@ package lab2;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("For the first problem in the homework:" + "\n");
+
         salut();
 
 
       float x = 24.5f;
       int y = 4;
       float sum = sum(x,y);
-        System.out.println("2. The result of the sum is: " + sum);
+        System.out.println("1.2  The result of the sum is: " + sum);
 
         x = 10;
         y = 3;
-        System.out.println("3. " + divide(x,y));
+        System.out.println("1.3  " + divide(x,y));
 
-        System.out.println("4. " + "\n" + "\t" + "a." + punctulA(5,8,6) + "\n" + "\t" + "b." + punctulB(55,9) +
+        System.out.println("1.4  " + "\n" + "\t" + "a." + punctulA(5,8,6) + "\n" + "\t" + "b." + punctulB(55,9) +
                 "\n" + "\t" + "c." +punctulC(20,3,5,8) +
                 "\n" + "\t" + "d." + punctulD(5,15,3,2,8) + "\n");
 
-
+        System.out.println("For the next part of the homework:" + "\n" + "\n" + "\n" + "2." + "\n");
 
 
 
       float subtraction = subtraction(x,y);
         System.out.println("The result of the subtraction is: " + subtraction);
+
+        int secondSum = sum(33,33);
+        System.out.println("The result of the sum is: "+ secondSum);
 
       float multiply = multiply(x,y);
         System.out.println("The result of the multiplication is: " + multiply);
@@ -32,11 +37,23 @@ public class Main {
       float divide = divide(x,y);
         System.out.println("The result of the division is: " + divide);
 
+        System.out.println("\n" + "3.");
+        writeJAVA();
+
 
     }
 
+    public static void writeJAVA () {
+        System.out.println("       J " + "   a   " + " V     V " + "   a   ");
+        System.out.println("       J " + "  a a  " + "  V   V  " + "  a a  ");
+        System.out.println(" J     J " + " aaaaa " + "   V V   " + " aaaaa ");
+        System.out.println("   J J   " + "a     a" + "    V    " + "a     a");
+    }
+
+
+
     public static void salut() {
-        System.out.println("Hello" + "\n" + "Andrei");
+        System.out.println("Hello" + "\n" + "\t" + "Andrei" + "\n");
     }
 
 

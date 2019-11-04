1889db225c005f06c2dccf744d6c45dd853fafbe
PopAndrei <popandrew90@gmail.com>
2019-11-04 12:45:24 +0200

    Arrays

--- src/lab5/Calculator/LogicalOP.java
+++ src/lab5/Calculator/LogicalOP.java
@@ -23,5 +23,33 @@ public class LogicalOP {
     public float divide (float first, float second, float third, float fourth) {return first/second/third/fourth; }
 
 
+    public void pozitii () {
+        int[] arPozitii = new int[100];
+        for (int i = 1; i <= 100; i++ ) {
+            arPozitii [i-1] = i;
+        }
+        for (int i = 0; i< 100; i ++){
+            System.out.print(arPozitii [i] + " ");
+        }
+    }
+
+    public void evenArray ( int [] evenarray) {
+        evenarray [0] = 2;
+        int counter = 0;
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+                evenarray [counter] = i;
+                counter ++;
+            }
+//        for (int i = 2; i <= evenarray.length && i % 2 == 0; i += 2) {
+//
+//            evenarray[i - 2] = i;
+        }
+        for (int i = 0; i < evenarray.length; i++) {
+            System.out.print(evenarray[i] + " ");
+
+        }
+    }
+
     //
 }
--- src/lab5/Calculator/Main.java
+++ src/lab5/Calculator/Main.java
@@ -10,7 +10,15 @@ public class Main {
         System.out.println(op.subtraction(100,10,10,10));
         System.out.println(op.multiply(5.5f,3.4f,10.5f));
         System.out.println(op.divide(1000,100,10,1));
-        System.out.println("\nExercitiul 2.");
+        System.out.println("\nExercitiul 2. \nValorile din acest array sunt: ");
+        op.pozitii();
+        System.out.println("\n\nExercitiul 3. \nValorile din acest array sunt: ");
+        int [] emptyArray = new int [50];
+        op.evenArray(emptyArray);
+        System.out.println("\n\nExercitiul 4. ");
+
+
+
 
 
 
